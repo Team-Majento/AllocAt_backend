7a7835e1d79d1a93ae1ab90c9447e4ac627594ae
thiunuwan <ravinduthiunuwan@gmail.com>
2023-06-05 16:21:04 +0530

    fix minor issue in img-upload

--- seat-allocation/seat-allocation-server/src/main/java/com/uom/seat/server/rest/controller/UploadFileController.java
+++ seat-allocation/seat-allocation-server/src/main/java/com/uom/seat/server/rest/controller/UploadFileController.java
@@ -5,23 +5,21 @@ import com.uom.seat.ImageUpload.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/file")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/file")
 public class UploadFileController {
 
     @Autowired
     private FileService fileService;
 
-    @PostMapping
+    @PostMapping()
     public ResponseEntity<Map<String,String>> uploadFile(@RequestParam("file")MultipartFile file){
         String publicUrl= fileService.uploadFile(file);
         Map<String,String> response = new HashMap<>();
