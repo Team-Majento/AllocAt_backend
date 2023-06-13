2b3fbfc047ec7f5a9b570e136e2df487778e0ff0
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-06-13 12:00:00 +0530

    add condition master data

--- seat-allocation/seat-allocation-engine/src/main/resources/liquibase/condition/master-data/condition.sql
+++ seat-allocation/seat-allocation-engine/src/main/resources/liquibase/condition/master-data/condition.sql
@@ -1,1 +1,8 @@
--- insert into condition_tbl(condition_name, discount_rate) values("january first",10)
+insert into condition_tbl(condition_name, discount_rate,date)
+values
+    ("January First",10,DATE '2023-06-14'),
+    ("Christmas",25,null),
+    ("April Sinhala-Tamil New Year",25,null),
+    ("Poya Day",15,null)
+
+
